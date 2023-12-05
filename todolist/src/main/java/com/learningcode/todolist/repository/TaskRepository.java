1045ee62e417bc9e94daec1acfa06d01a41fb2d4
Kayla <harris.kaylamarie@gmail.com>
2023-12-05 14:30:48 -0600

    Partial repo setup

--- todolist/src/main/java/com/learningcode/todolist/repository/TaskRepository.java
+++ todolist/src/main/java/com/learningcode/todolist/repository/TaskRepository.java
@@ -1,8 +1,14 @@
 package com.learningcode.todolist.repository;
 
 // Houses classes that interact with the database, usually using Spring Data JPA.
+// In other words this is where we connect to the database
 
 
-public interface TaskRepository {
+import com.learningcode.todolist.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Integer> {
 
 }
