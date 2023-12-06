c9ff606d4e56abbdb6b9cfdeccca2b532f281cfb
Kayla <harris.kaylamarie@gmail.com>
2023-12-06 17:17:00 -0600

    made sure it runs properly on main

--- todolist/src/main/java/com/learningcode/todolist/service/CategoryServiceImpl.java
+++ todolist/src/main/java/com/learningcode/todolist/service/CategoryServiceImpl.java
@@ -3,7 +3,9 @@ package com.learningcode.todolist.service;
 import com.learningcode.todolist.model.Category;
 import com.learningcode.todolist.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class
 CategoryServiceImpl implements CategoryService {
 
@@ -12,6 +14,7 @@ CategoryServiceImpl implements CategoryService {
 
     @Override
     public Category createCategory(Category category){
+
         return categoryRepository.save(category);
     }
 }
--- todolist/src/main/java/com/learningcode/todolist/service/TaskServiceImpl.java
+++ todolist/src/main/java/com/learningcode/todolist/service/TaskServiceImpl.java
@@ -3,7 +3,9 @@ package com.learningcode.todolist.service;
 import com.learningcode.todolist.model.Task;
 import com.learningcode.todolist.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TaskServiceImpl implements TaskService {
 
     // Marks a constructor, field, or method to be autowired by Spring's dependency injection. This annotation removes the need for explicit setter methods and can be used on a constructor to autowire the constructor itself.
