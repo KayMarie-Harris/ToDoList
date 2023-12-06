c481224a638b62005107ef758ed0a9f8a3b714ca
Kayla <harris.kaylamarie@gmail.com>
2023-12-06 16:01:53 -0600

    added service annotation

--- todolist/src/main/java/com/learningcode/todolist/service/CategoryServiceImpl.java
+++ todolist/src/main/java/com/learningcode/todolist/service/CategoryServiceImpl.java
@@ -3,9 +3,10 @@ package com.learningcode.todolist.service;
 import com.learningcode.todolist.model.Category;
 import com.learningcode.todolist.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class
-CategoryServiceImpl implements CategoryService {
+@Service // Indicates that a class is a service
+public class CategoryServiceImpl implements CategoryService {
 
     @Autowired
     private CategoryRepository categoryRepository;
